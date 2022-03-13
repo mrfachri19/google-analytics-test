84d30ba662dd6430c82d185f4e1e997c55af7938
Fachri Maulana <fachryfachry1997@gmail.com>
2022-03-13 23:53:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
+import ReactGa from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGa.initialize('G-4C946Y21Z7');
+    // repeat view page
+    ReactGa.pageview("/");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World !</h1>
       </header>
     </div>
   );
